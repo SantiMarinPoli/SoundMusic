c5fe12c7c40715431254cfbe4fa0d5240c400c77
Santiago Medina <medinas@visualsystems.com>
2018-09-18 08:04:11 -0500

    IAlbumDao y AlbumDaoImpl

---
+++ src/java/co/com/soundMusic/Artista/Album/AlbumDaoImpl.java
@@ -0,0 +1,48 @@
+package java.co.com.soundMusic.Artista.Album;
+
+import co.com.soundMusic.utilidades.DBUtil;
+import java.co.com.soundMusic.Contacto.Ciudad;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Santiago Medina Pelaez
+ */
+public class AlbumDaoImpl implements IAlbumDao {
+    private Connection conexion;
+    private CiudadDaoImpl ciudad;
+    private ArtistaDaoImpl artista;
+
+    public AlbumDaoImpl() {
+        conexion = DBUtil.getConexion();
+        ciudad = new CiudadDaoImpl();
+        artista = new ArtistaDaoImpl();
+    }
+
+    @Override
+    public List<Album> obtenerAlbumes() throws SQLException {
+    }
+
+    @Override
+    public Album obtenerAlbum(int idAlbum) throws SQLException {
+    }
+
+    @Override
+    void crearAlbum(Album album) throws SQLException {
+    }
+
+    @Override
+    public void eliminarAlbum(String estado, int idAlbum) throws SQLException {
+    }
+
+    @Override
+    public void actualizarAlbum(Album album) throws SQLException {
+    }
+}
