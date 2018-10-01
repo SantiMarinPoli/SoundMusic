d048c67aa4b1da183cebf349542cecc671d16365
Santiago Medina <medinas@visualsystems.com>
2018-10-01 11:28:15 -0500

    Version 1.8.1- Pruebas Unitarias

--- test/co/com/soundMusic/EmpresaDifusora/EmpresaDifusoraDaoImplTest.java
+++ test/co/com/soundMusic/EmpresaDifusora/EmpresaDifusoraDaoImplTest.java
@@ -1,65 +1,77 @@
 package co.com.soundMusic.EmpresaDifusora;
 
+import java.sql.Date;
+import java.sql.SQLException;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 
 /**
  *
  * @author Santiago Medina Pelaez
  */
 public class EmpresaDifusoraDaoImplTest {
-    
+
+    EmpresaDifusora empresaPrueba, empresaPruebaAct;
+    List<EmpresaDifusora> pruebaLstEmpresa;
+    EmpresaDifusoraDaoImpl daoEmpresaDifusora;
+
     public EmpresaDifusoraDaoImplTest() {
     }
-    
+
     @BeforeClass
-    public static void setUpClass() {
+    public static void setUpClass() throws Exception {
     }
-    
+
     @AfterClass
-    public static void tearDownClass() {
+    public static void tearDownClass() throws Exception {
     }
-    
+
     @Before
-    public void setUp() {
+    public void setUp() throws SQLException {
+        daoEmpresaDifusora = new EmpresaDifusoraDaoImpl();
+        empresaPrueba = new EmpresaDifusora(1, "YOUTUBE", Date.valueOf("2018-06-01"), null, "A",
+                1, 1, 1);
+        empresaPruebaAct = new EmpresaDifusora(1, "YOUTUBE", Date.valueOf("2018-06-01"), null, "A",
+                1, 1, 1);
     }
-    
+
     @After
     public void tearDown() {
     }
 
     /**
      * Test of obtenerEmpresasDifusoras method, of class EmpresaDifusoraDaoImpl.
+     *
+     * @throws java.lang.Exception
      */
     @Test
     public void testObtenerEmpresasDifusoras() throws Exception {
         System.out.println("obtenerEmpresasDifusoras");
-        EmpresaDifusoraDaoImpl instance = null;
-        List<EmpresaDifusora> expResult = null;
-        List<EmpresaDifusora> result = instance.obtenerEmpresasDifusoras();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        List<EmpresaDifusora> resultadoEsperado = null;
+        List<EmpresaDifusora> resultadoActual = daoEmpresaDifusora.obtenerEmpresasDifusoras();
+        assertFalse(resultadoActual.isEmpty());
     }
 
     /**
      * Test of obtenerEmpresaDifusora method, of class EmpresaDifusoraDaoImpl.
+     *
+     * @throws java.lang.Exception
      */
     @Test
     public void testObtenerEmpresaDifusora() throws Exception {
         System.out.println("obtenerEmpresaDifusora");
-        int idEmpresaDifusora = 0;
-        EmpresaDifusoraDaoImpl instance = null;
-        EmpresaDifusora expResult = null;
-        EmpresaDifusora result = instance.obtenerEmpresaDifusora(idEmpresaDifusora);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        int idEmpresaDifusora = 1;
+        EmpresaDifusora resultadoActual = daoEmpresaDifusora.obtenerEmpresaDifusora(idEmpresaDifusora);
+        assertEquals(empresaPrueba.getIdEmpresaDifusora(), resultadoActual.getIdEmpresaDifusora());
+        assertEquals(empresaPrueba.getNombre(), resultadoActual.getNombre());
+        assertEquals(empresaPrueba.getStatus(), resultadoActual.getStatus());
+
     }
 
     /**
@@ -68,11 +80,13 @@ public class EmpresaDifusoraDaoImplTest {
     @Test
     public void testCrearEmpresaDifusora() throws Exception {
         System.out.println("crearEmpresaDifusora");
-        EmpresaDifusora empresaDifusora = null;
-        EmpresaDifusoraDaoImpl instance = null;
-        instance.crearEmpresaDifusora(empresaDifusora);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        daoEmpresaDifusora.crearEmpresaDifusora(empresaPrueba);
+        int idEmpresaDifusoraPrueba = daoEmpresaDifusora.getUltimoIdEmpresaDifusora();
+        EmpresaDifusora empresaEsperado = empresaPrueba;
+        empresaEsperado.setIdEmpresaDifusora(idEmpresaDifusoraPrueba);
+        EmpresaDifusora empresaActual = daoEmpresaDifusora.obtenerEmpresaDifusora(idEmpresaDifusoraPrueba);
+
+        assertEquals(empresaEsperado, empresaActual);
     }
 
     /**
@@ -81,25 +95,44 @@ public class EmpresaDifusoraDaoImplTest {
     @Test
     public void testEliminarEmpresaDifusora() throws Exception {
         System.out.println("eliminarEmpresaDifusora");
-        String status = "";
-        int idEmpresaDifusora = 0;
-        EmpresaDifusoraDaoImpl instance = null;
-        instance.eliminarEmpresaDifusora(status, idEmpresaDifusora);
+        String status = "I";
+        int idEmpresaDifusora = 1;
+        EmpresaDifusora empresaDifusoraPruebaElm = empresaPrueba;
+        empresaDifusoraPruebaElm.setStatus("I");
+        daoEmpresaDifusora.eliminarEmpresaDifusora(status, idEmpresaDifusora);
+        EmpresaDifusora resultadoActual = daoEmpresaDifusora.obtenerEmpresaDifusora(idEmpresaDifusora);
+
+        assertEquals(empresaDifusoraPruebaElm, resultadoActual);
+    }
+
+    /**
+     * Test of actualizarEmpresaDifusora method, of class
+     * EmpresaDifusoraDaoImpl.
+     */
+    @Test
+    @Ignore
+    public void testActualizarEmpresaDifusora() throws Exception {
+        System.out.println("actualizarEmpresaDifusora");
+
+        daoEmpresaDifusora.actualizarEmpresaDifusora(empresaPruebaAct);
+        EmpresaDifusora resultadoActual = daoEmpresaDifusora.obtenerEmpresaDifusora(1);
+
+        assertEquals(empresaPruebaAct, resultadoActual);
+    }
+
+    /**
+     * Test of getUltimoIdEmpresaDifusora method, of class
+     * EmpresaDifusoraDaoImpl.
+     */
+    @Test
+    public void testGetUltimoIdEmpresaDifusora() throws Exception {
+        System.out.println("getUltimoIdEmpresaDifusora");
+        EmpresaDifusoraDaoImpl instance = new EmpresaDifusoraDaoImpl();
+        int expResult = 0;
+        int result = instance.getUltimoIdEmpresaDifusora();
+        assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
 
-    /**
-     * Test of actualizarEmpresaDifusora method, of class EmpresaDifusoraDaoImpl.
-     */
-    @Test
-    public void testActualizarEmpresaDifusora() throws Exception {
-        System.out.println("actualizarEmpresaDifusora");
-        EmpresaDifusora empresaDifusora = null;
-        EmpresaDifusoraDaoImpl instance = null;
-        instance.actualizarEmpresaDifusora(empresaDifusora);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
---
+++ test/co/com/soundMusic/Negocio/Regalias/RegaliaTest.java
@@ -0,0 +1,72 @@
+package co.com.soundMusic.Negocio.Regalias;
+
+import co.com.soundMusic.Artista.Artista;
+import co.com.soundMusic.Contacto.Ciudad.Ciudad;
+import co.com.soundMusic.Contacto.Contacto;
+import co.com.soundMusic.Contacto.Pais.Pais;
+import co.com.soundMusic.EmpresaDifusora.EmpresaDifusora;
+import co.com.soundMusic.EmpresaDifusora.TipoCosto.CostoActividad;
+import co.com.soundMusic.Negocio.Regalias.ArtistaEmpresa.ArtistaEmpresa;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author medinas
+ */
+public class RegaliaTest {
+
+    ArtistaEmpresa artistaEmpresa;
+    Artista artista;
+    Contacto contacto;
+    Ciudad ciudad;
+    Pais pais;
+    EmpresaDifusora empresaDifusora;
+    CostoActividad costo;
+
+    public RegaliaTest() {
+    }
+
+    @Before
+    public void setUp() throws SQLException,InvocationTargetException {
+        pais = new Pais(1, "COLOMBIA");
+        ciudad = new Ciudad(1, "MEDELLIN", pais);
+
+        String[] datosContacto = {"3164349117", "5836395", "MI CASA", "MI BARRIO", "SANMEDI@45.COM"};
+        contacto = new Contacto(1, datosContacto, ciudad);
+
+        String[] datosArtista = {"SANTIAGO", null, "MEDINA", "PELAEZ", "MC DINA", "M", "A"};
+        Date[] fechasArtista = {Date.valueOf("1992-06-01"), Date.valueOf(LocalDate.now())};
+
+        artista = new Artista(1, datosArtista, fechasArtista, contacto);
+
+        empresaDifusora = new EmpresaDifusora(1, "YOUTUBE", Date.valueOf("2018-06-01"), null, "A", 1, 1, 1);
+        artistaEmpresa = new ArtistaEmpresa(1, artista, empresaDifusora);
+
+        costo = new CostoActividad(1, 10, Date.valueOf("2018-08-08"), null);
+
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of calcularRegalias method, of class Regalia.
+     */
+    @Test
+    public void testCalcularRegalias() {
+        System.out.println("calcularRegalias");
+        Regalia regaliaPrueba = new Regalia(1, 0.0f, 10, Date.valueOf("2018-08-05"), artistaEmpresa, costo);
+        float resultadoEsperado = 100F;
+        float resultadoActual = regaliaPrueba.calcularRegalias();
+        assertEquals(resultadoEsperado, resultadoActual, 0.0);
+    }
+
+}
