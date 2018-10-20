d08713b65b9a3625776ec2cfb4a5ae24df2b4b54
Sanmedi27 <sanme@Santiago>
2018-10-19 21:23:52 -0500

    V 2.7 Se exporta la base de datos a archivo XML para hacer pruebas unitarias.

--- src/java/co/com/soundMusic/utilidades/DatabaseExportSample.java
+++ src/java/co/com/soundMusic/utilidades/DatabaseExportSample.java
@@ -2,40 +2,49 @@ package co.com.soundMusic.utilidades;
 
 import java.io.FileOutputStream;
 import java.sql.Connection;
+import org.dbunit.database.AmbiguousTableNameException;
+import org.dbunit.database.DatabaseConfig;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.database.QueryDataSet;
-import org.dbunit.database.search.TablesDependencyHelper;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.ext.oracle.OracleDataTypeFactory;
 
 /**
  *
  * @author Santiago Medina Peláez
  */
 public class DatabaseExportSample {
-    public static void main(String[] args) throws Exception
-    {
-        // database connection
+
+    public static void main(String[] args) throws Exception {
+        // database connection        
+        Class driverClass = Class.forName("oracle.jdbc.OracleDriver");
         Connection jdbcConnection = DBUtil.getConexion();
         IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
 
+        DatabaseConfig config = connection.getConfig();
+        config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new OracleDataTypeFactory());
+
         // partial database export
-        QueryDataSet partialDataSet = new QueryDataSet(connection);
-        partialDataSet.addTable("FOO", "SELECT * FROM TABLE WHERE COL='VALUE'");
-        partialDataSet.addTable("BAR");
-        FlatXmlDataSet.write(partialDataSet, new FileOutputStream("partial.xml"));
+        partialExport(connection);
 
         // full database export
-        IDataSet fullDataSet = connection.createDataSet();
-        FlatXmlDataSet.write(fullDataSet, new FileOutputStream("full.xml"));
-        
-        // dependent tables database export: export table X and all tables that
-        // have a PK which is a FK on X, in the right order for insertion
-        String[] depTableNames = 
-          TablesDependencyHelper.getAllDependentTables( connection, "X" );
-        IDataSet depDataset = connection.createDataSet( depTableNames );
-        FlatXmlDataSet.write(depDataset, new FileOutputStream("dependents.xml"));          
-        
+        fullExport(connection);
+    }
+
+    private static void partialExport(IDatabaseConnection connection)
+            throws AmbiguousTableNameException, Exception {
+        QueryDataSet partialDataSet = new QueryDataSet(connection);
+        //partialDataSet.addTable("FOO", "SELECT * FROM TABLE WHERE COL='VALUE'");
+        partialDataSet.addTable("LOG_AUDITORIA");
+        FlatXmlDataSet.write(partialDataSet, new FileOutputStream("test/dataBase/partial.xml"));
+    }
+
+    private static void fullExport(IDatabaseConnection connection)
+            throws AmbiguousTableNameException, Exception {
+        String[] nombreTablas = {"ARTISTA", "EMPRESA_DIFUSORA", "USUARIO", "LOG_AUDITORIA"};
+        IDataSet fullDataSet = connection.createDataSet(nombreTablas);
+        FlatXmlDataSet.write(fullDataSet, new FileOutputStream("test/dataBase/full.xml"));
     }
 }
